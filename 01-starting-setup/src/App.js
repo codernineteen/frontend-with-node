101586a6ea03b65ec884ac04ffd3a8a289198b34
Yechan  Jeon <65900882+codernineteen@users.noreply.github.com>
2021-12-17 15:32:00 +0900

    react basics: components

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also tag!</p>
+      <p>This is also tag!</p>
+    </div>
+  );
+}
+
+export default App;
